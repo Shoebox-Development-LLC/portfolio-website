efd0b0354d8d5fcd12cb27d70042bfd666efcc77
raked <raked@users.noreply.github.com>
2021-08-05 14:51:53 -0400

    Added Title component
    Need to remove inline styles and replace with definitions on index.css, but don't feel like it right now.

---
+++ src/components/Title.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// styles
+
+const titleStyle = {
+    minHeight: "216px",
+    backgroundColor: "#C4C4C4"
+}
+
+// markup
+
+const Title = () => {
+    return (
+        <div style={titleStyle}>
+            content heres
+        </div>
+    )
+}
+
+export default Title
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import * as React from "react"
 import "../styles/index.css"
 import Nav from "../components/Nav"
+import Title from "../components/Title"
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>Shoebox Development LLC</title>
+      <header>
       <Nav/>
+      <Title/>
+      </header>
       enter content here sam
     </main>
   )
