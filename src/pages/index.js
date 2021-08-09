d51292a4355467e92895dcf1822fd3b34f99f862
raked <raked@users.noreply.github.com>
2021-08-09 09:52:31 -0400

    Added showcase and spacers

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import "../styles/index.css"
 import Nav from "../components/Nav"
 import Title from "../components/Title"
+import Spacer from "../components/Spacer"
+import Showcase from "../components/Showcase"
 
 // markup
 const IndexPage = () => {
@@ -11,6 +13,9 @@ const IndexPage = () => {
       <header>
       <Nav/>
       <Title/>
+      <Spacer/>
+      <Showcase/>
+      <Spacer/>
       </header>
       enter content here sam
     </main>
