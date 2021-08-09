e7d69daa1d588efb1c6ea92b6c60982b627c2556
raked <raked@users.noreply.github.com>
2021-08-09 10:52:24 -0400

    Finished showcase
    Just need assets

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import ShowcaseItem from "./ShowcaseItem"
 
 const Showcase = () => {
     return (
         <div className="showcase">
-            
+            <div className="showcaseBackground"></div>
+            <ShowcaseItem text="Web Development"/>
+            <ShowcaseItem text="App Development"/>
+            <ShowcaseItem text="IT Department"/>
+            <ShowcaseItem text="Blockchain"/>
+            <ShowcaseItem text="Other Service"/>
+            <ShowcaseItem text="Other Service"/>
         </div>
     )
 }
---
+++ src/components/ShowcaseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowcaseItem = ({ text }) => {
+    return (
+        <div className="showcaseItem">
+            <span className="showcaseItemText">{text}</span>
+        </div>
+    )
+}
+
+export default ShowcaseItem
