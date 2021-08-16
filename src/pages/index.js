155125ea496649574f7c6d9340ba1816a7e6c686
raked <raked@users.noreply.github.com>
2021-08-16 14:18:40 -0400

    Made more presentable

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,14 +25,14 @@ const IndexPage = () => {
         <LeftCaption
           header='Our Story'
           text='This is a sample block of text that will be replaced some time in the
-future. pppppppeeennnnnniisssss'
+future. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin nunc ex. Quisque ut ante vel nisi malesuada luctus at in nisl. Suspendisse potenti. Sed at neque id risus iaculis commodo. Morbi nec ligula ante. Duis ac dui elit. Cras sit amet dolor laoreet, vestibulum erat a, laoreet libero. Vivamus mollis consectetur nisl, sed dictum arcu pharetra eget. Vivamus laoreet tellus sem, non hendrerit augue elementum eu. Sed vel mollis est, vehicula tincidunt tortor. Curabitur faucibus mauris a risus pellentesque, sed finibus turpis efficitur. Quisque vehicula, orci eu suscipit luctus, libero mauris molestie lectus, vitae convallis est risus eget sem. '
           idCircle='greyCircle0'
           idGreyBox='greyBox20'
         />
         <Spacer />
         <LeftCaption
           header='Our Mission'
-          text='This is a sample block of text that will be replaced some time in the future. hello this is breaking the design also why does the word break like that'
+          text='This is a sample block of text that will be replaced some time in the future.'
           idCircle='greyCircle1'
           idLeftCaption='leftCaption1'
           idLeftCaptionCaption='leftCaptionCaption1'
