0299f5c38b805e3b35cc65a7ec732d967b15cdab
raked <raked@users.noreply.github.com>
2021-08-25 11:53:56 -0400

    Added code-splitting using Suspense and React.lazy()

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
-import * as React from "react"
+// import
+import React, { Suspense } from "react"
 import "../styles/index.css"
 import Nav from "../components/Nav"
 import Title from "../components/Title"
 import Spacer from "../components/Spacer"
 import Showcase from "../components/Showcase"
-import LeftCaption from "../components/LeftCaption"
 import MotivationalText from "../components/MotivationalText"
-import Footer from "../components/Footer"
 import Helmet from "react-helmet"
+
+// lazy components
+const LeftCaption = React.lazy(() => import("../components/LeftCaption"))
+const Footer = React.lazy(() => import("../components/Footer"))
 
 // markup
 const IndexPage = () => {
   return (
     <>
-      <Helmet htmlAttributes={{ lang: "en" }}>
+      <Helmet>
+        <html lang='en' />
         <title>Shoebox Development LLC</title>
         <meta
           name='description'
@@ -30,28 +34,32 @@ const IndexPage = () => {
         <Spacer />
       </header>
       <main>
-        <LeftCaption
-          header='Our Story'
-          text='This is a sample block of text that will be replaced some time in the
+        <Suspense fallback={<div>Loading...</div>}>
+          <LeftCaption
+            header='Our Story'
+            text='This is a sample block of text that will be replaced some time in the
 future. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin nunc ex. Quisque ut ante vel nisi malesuada luctus at in nisl. Suspendisse potenti. Sed at neque id risus iaculis commodo. Morbi nec ligula ante. Duis ac dui elit. Cras sit amet dolor laoreet, vestibulum erat a, laoreet libero. Vivamus mollis consectetur nisl, sed dictum arcu pharetra eget. Vivamus laoreet tellus sem, non hendrerit augue elementum eu. Sed vel mollis est, vehicula tincidunt tortor. Curabitur faucibus mauris a risus pellentesque, sed finibus turpis efficitur. Quisque vehicula, orci eu suscipit luctus, libero mauris molestie lectus, vitae convallis est risus eget sem.'
-          idCircle='greyCircle0'
-          idGreyBox='greyBox20'
-          idLeftCaptionContent='leftCaptionContent0'
-        />
-        <Spacer />
-        <LeftCaption
-          header='Our Mission'
-          text='This is a sample block of text that will be replaced some time in the future. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin nunc ex. Quisque ut ante vel nisi malesuada luctus at in nisl. Suspendisse potenti. Sed at neque id risus iaculis commodo. Morbi nec ligula ante. Duis ac dui elit. Cras sit amet dolor laoreet, vestibulum erat a, laoreet libero. Vivamus mollis consectetur nisl, sed dictum arcu pharetra eget. Vivamus laoreet tellus sem, non hendrerit augue elementum eu. Sed vel mollis est, vehicula tincidunt tortor. Curabitur faucibus mauris a risus pellentesque, sed finibus turpis efficitur. Quisque vehicula, orci eu suscipit luctus, libero mauris molestie lectus, vitae convallis est risus eget sem.'
-          idCircle='greyCircle1'
-          idLeftCaption='leftCaption1'
-          idLeftCaptionCaption='leftCaptionCaption1'
-          idGreyBox='greyBox21'
-          idLeftCaptionContent='leftCaptionContent1'
-        />
+            idCircle='greyCircle0'
+            idGreyBox='greyBox20'
+            idLeftCaptionContent='leftCaptionContent0'
+          />
+          <Spacer />
+          <LeftCaption
+            header='Our Mission'
+            text='This is a sample block of text that will be replaced some time in the future. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin nunc ex. Quisque ut ante vel nisi malesuada luctus at in nisl. Suspendisse potenti. Sed at neque id risus iaculis commodo. Morbi nec ligula ante. Duis ac dui elit. Cras sit amet dolor laoreet, vestibulum erat a, laoreet libero. Vivamus mollis consectetur nisl, sed dictum arcu pharetra eget. Vivamus laoreet tellus sem, non hendrerit augue elementum eu. Sed vel mollis est, vehicula tincidunt tortor. Curabitur faucibus mauris a risus pellentesque, sed finibus turpis efficitur. Quisque vehicula, orci eu suscipit luctus, libero mauris molestie lectus, vitae convallis est risus eget sem.'
+            idCircle='greyCircle1'
+            idLeftCaption='leftCaption1'
+            idLeftCaptionCaption='leftCaptionCaption1'
+            idGreyBox='greyBox21'
+            idLeftCaptionContent='leftCaptionContent1'
+          />
+        </Suspense>
         <Spacer />
       </main>
       <footer>
-        <Footer />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Footer />
+        </Suspense>
       </footer>
     </>
   )
